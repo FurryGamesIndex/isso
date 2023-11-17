088c7a54c0d1bc2710b35a73bdc7269d96ea9ff3
Eatswap <root@eatswap.org>
2023-11-18 02:24:12 +0800

    feat: stop manipulating IP address

--- isso/utils/__init__.py
+++ isso/utils/__init__.py
@@ -19,19 +19,7 @@ def anonymize(remote_addr):
     and /48 (zero'd).
 
     """
-    if not isinstance(remote_addr, str) and isinstance(remote_addr, str):
-        remote_addr = remote_addr.decode('ascii', 'ignore')
-    try:
-        ipv4 = ipaddress.IPv4Address(remote_addr)
-        return u''.join(ipv4.exploded.rsplit('.', 1)[0]) + '.' + '0'
-    except ipaddress.AddressValueError:
-        try:
-            ipv6 = ipaddress.IPv6Address(remote_addr)
-            if ipv6.ipv4_mapped is not None:
-                return anonymize(str(ipv6.ipv4_mapped))
-            return u'' + ipv6.exploded.rsplit(':', 5)[0] + ':' + ':'.join(['0000'] * 5)
-        except ipaddress.AddressValueError:
-            return u'0.0.0.0'
+    return remote_addr
 
 
 class Bloomfilter:
